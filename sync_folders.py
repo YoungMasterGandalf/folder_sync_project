56488d86c9b344619ea94cde68486d961d28929c
Daniel Chmúrny <daniel123chmurny@gmail.com>
2024-08-04 15:35:55 +0200

    Implement general sync logic

--- sync_folders.py
+++ sync_folders.py
@@ -1,4 +1,9 @@
+import os
 import argparse
+import logging
+import filecmp
+import shutil
+import time
 
 
 def main():
@@ -34,6 +39,51 @@ def main():
     replica_path = args.replica
     sync_interval = args.sync_interval
     log_file = args.log_file
+
+    while True:
+        try:
+            for source_dir, subdirs, filenames in os.walk(source_path):
+                replica_dir = source_dir.replace(source_path, replica_path, 1)
+
+                if not os.path.exists(replica_dir):
+                    os.makedirs(replica_dir)
+                    logger.info(f"Replica directory CREATED: {replica_dir}.")
+
+                for filename in filenames:
+                    source_file = os.path.join(source_dir, filename)
+                    replica_file = os.path.join(replica_dir, filename)
+
+                    if not os.path.exists(replica_file) or not filecmp.cmp(
+                        source_file, replica_file
+                    ):
+                        shutil.copy2(source_file, replica_file)
+                        logger.info(
+                            f"Source file COPIED: {source_file}. New replica: {replica_file}."
+                        )
+
+            for replica_dir, subdirs, filenames in os.walk(replica_path, topdown=False):
+                source_dir = replica_dir.replace(replica_path, source_path, 1)
+
+                for filename in filenames:
+                    source_file = os.path.join(source_dir, filename)
+
+                    if not os.path.exists(source_file):
+                        replica_file = os.path.join(replica_dir, filename)
+                        os.remove(replica_file)
+                        logger.info(f"Replica file DELETED: {replica_file}.")
+
+                if not os.path.exists(source_dir):
+                    os.rmdir(replica_dir)
+                    logger.info(f"Replica directory DELETED: {replica_dir}.")
+
+            time.sleep(sync_interval)
+        except KeyboardInterrupt:
+            logger.info("KeyboardInterrupt: folder synchronization stopped.")
+            break
+        except Exception as e:
+            logger.error(e)
+            break
+
 
 if __name__ == "__main__":
     main()
