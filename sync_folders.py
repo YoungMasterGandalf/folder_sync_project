fda2649fdf318e1bb6d9ac0d46fd31c823b4b4b3
Daniel Chmúrny <daniel123chmurny@gmail.com>
2024-08-04 15:44:03 +0200

    Shorten arg parser code

--- sync_folders.py
+++ sync_folders.py
@@ -8,20 +8,10 @@ import time
 
 def main():
     parser = argparse.ArgumentParser()
-
-    source_help = "Source folder path, e.g. '/Users/JohnDoe/Documents/source'."
-    parser.add_argument("source", type=str, help=source_help)
-
-    replica_help = (
-        "Replica folder path, e.g. '/Users/JohnDoe/Documents/source_replica'."
-    )
-    parser.add_argument("replica", type=str, help=replica_help)
-
-    sync_interval_help = "Synchronization interval in seconds, e.g. '5' will result in sync each 5 seconds."
-    parser.add_argument("sync_interval", type=int, help=sync_interval_help)
-
-    log_file_help = "Path to a file storing the log output, e.g. '/Users/JohnDoe/Documents/logs.json'."
-    parser.add_argument("log_file", type=str, help=log_file_help)
+    parser.add_argument("source", type=str, help="Source folder path.")
+    parser.add_argument("replica", type=str, help="Replica folder path.")
+    parser.add_argument("sync_interval", type=int, help="Synchronization interval in seconds.")
+    parser.add_argument("log_file", type=str, help="Path to log file.")
 
     args = parser.parse_args()
 
