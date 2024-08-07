2df14df4a509ce8a19e375b4a3181111bac8de45
Daniel Chmúrny <daniel123chmurny@gmail.com>
2024-08-07 10:31:05 +0200

    Add basic logger formatter

--- sync_folders.py
+++ sync_folders.py
@@ -23,7 +23,14 @@ def main():
 
     logger = logging.getLogger(__name__)
     logger.setLevel(logging.INFO)
+
+    formatter = logging.Formatter(
+        "%(asctime)s | %(levelname)s | %(name)s | %(message)s"
+    )
+
     console_handler = logging.StreamHandler()
+    console_handler.setFormatter(formatter)
+
     logger.addHandler(console_handler)
 
     replica_path = args.replica
